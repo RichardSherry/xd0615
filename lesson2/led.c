3384f33051c6ca4c86644dcc5a1720856a0fa856
RichardSherry <731705278@qq.com>
2020-06-17 00:20:41 +0800

    新建了lesson2并整理了仓库

---
+++ lesson2/led.c
@@ -0,0 +1,16 @@
+void setup()
+{
+  for(int i=0;i<8;i++){
+    pinMode(i,OUTPUT);
+  }
+}
+
+void loop()
+{
+  for(int i=0;i<8;i++){
+  digitalWrite(i, HIGH);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(i, LOW);
+  delay(500); // Wait for 1000 millisecond(s)
+  }
+}
