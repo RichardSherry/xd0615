ff25e8497310f3e6bfd34af234f7524d3f0ff2cf
RichardSherry <731705278@qq.com>
2020-06-17 17:00:44 +0800

    完成了lesson2

--- lesson2/led.c
+++ lesson2/led.c
@@ -1,16 +1,23 @@
+int i;
 void setup()
 {
-  for(int i=0;i<8;i++){
+  for(i=0;i<8;i++){
     pinMode(i,OUTPUT);
   }
 }
 
 void loop()
 {
-  for(int i=0;i<8;i++){
-  digitalWrite(i, HIGH);
-  delay(500); // Wait for 1000 millisecond(s)
-  digitalWrite(i, LOW);
-  delay(500); // Wait for 1000 millisecond(s)
+  for(i=0;i<8;i=i+2){
+    digitalWrite(i, HIGH);
+    delay(500); // Wait for 1000 millisecond(s)
+    digitalWrite(i, LOW);
+    delay(500); // Wait for 1000 millisecond(s)
+  }
+  for(i=1;i<8;i=i+2){
+    digitalWrite(i,HIGH);
+    delay(200);
+    digitalWrite(i,LOW);
+    delay(500);
   }
 }
